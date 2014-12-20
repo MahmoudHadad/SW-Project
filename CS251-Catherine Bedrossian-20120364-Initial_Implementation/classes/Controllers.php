e73e8de29ef46e526c87014d9b73245684b710f2
hooda hadad <hooda_fci@yahoo.com>
2014-12-20 20:24:38 +0200

    presentation added

---
+++ CS251-Catherine Bedrossian-20120364-Initial_Implementation/classes/Controllers.php
@@ -0,0 +1,107 @@
+<?php 
+/**
+* 
+*/
+class AdController
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function Create(ad_id)
+	{
+		# code...
+	}
+
+	public function Destroy(ad_id)
+	{
+		# code...
+	}
+
+	public function GetAdInfo(ad_id)
+	{
+		# code...
+	}
+	public function GetAd(ad_id)
+	{
+		# code...
+	}
+
+}
+
+/**
+* 
+*/
+class AdListController
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function Create(AdList)
+	{
+		# code...
+	}
+
+	public function Destroy(ad_list_id)
+	{
+		# code...
+	}
+
+	public function Rename(ad_list_id,string)
+	{
+		# code...
+	}
+
+	public function ViewList(user_id)
+	{
+		# code...
+	}
+
+	public function AddToTheList(ad_list_id,Ad)
+	{
+		# code...
+	}
+}
+
+/**
+* 
+*/
+class UserController
+{
+		
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function Create(ad_id)
+	{
+		# code...
+	}
+
+	public function Destroy(ad_id)
+	{
+		# code...
+	}
+
+	public function GetPersonalInfo(ad_id)
+	{
+		# code...
+	}
+
+	public function LogIn()
+	{
+		# code...
+	}
+
+	public function LogOut()
+	{
+		# code...
+	}
+}
+ ?>
